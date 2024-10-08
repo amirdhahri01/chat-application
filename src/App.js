37bed9b118eb6198b8285a66aad810750a83a90b
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-08 18:48:53 +0100

    The creation of the chat engine is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h>Hello world</h>
+      <ChatEngine
+        height="100vh"
+        projectID="1ce2707a-58fb-4346-971c-7a8367f4f8df"
+        userName="AmirDhahri"
+        userSecret="azerty123"
+      />
     </div>
   );
 }
